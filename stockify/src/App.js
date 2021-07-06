f202ebd115cb6cbf588af7ee59098c8919610a8c
ItsVroo <tarunr0681@gmail.com>
2021-07-06 21:49:05 +0530

    Front End Done

--- stockify/src/App.js
+++ stockify/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Stockheader from './Components/stocklist/Stockheader';
-import './better.css';
+import './table.css';
+import './header.css';
 import Stocklist from './Components/stocklist/Stocklist';
 
 const App = () => {
@@ -36,23 +37,151 @@ const App = () => {
     Lowval: 50,
     Higval: 150
    }
+
+   ,
+   {
+    Name: 'A',
+    Curval: 100,
+    Lowval: 50,
+    Higval: 150
+   }
+   ,
+   {
+    Name: 'A',
+    Curval: 100,
+    Lowval: 50,
+    Higval: 150
+   }
+   ,
+   {
+    Name: 'A',
+    Curval: 100,
+    Lowval: 50,
+    Higval: 150
+   }
+   ,
+   {
+    Name: 'A',
+    Curval: 100,
+    Lowval: 50,
+    Higval: 150
+   }
+   ,
+   {
+    Name: 'A',
+    Curval: 100,
+    Lowval: 50,
+    Higval: 150
+   }
+   ,
+   {
+    Name: 'A',
+    Curval: 100,
+    Lowval: 50,
+    Higval: 150
+   }
+   ,
+   {
+    Name: 'A',
+    Curval: 100,
+    Lowval: 50,
+    Higval: 150
+   }
+   ,
+   {
+    Name: 'A',
+    Curval: 100,
+    Lowval: 50,
+    Higval: 150
+   }
+   ,
+   {
+    Name: 'A',
+    Curval: 100,
+    Lowval: 50,
+    Higval: 150
+   }
+   ,
+   {
+    Name: 'A',
+    Curval: 100,
+    Lowval: 50,
+    Higval: 150
+   }
+   ,
+   {
+    Name: 'A',
+    Curval: 100,
+    Lowval: 50,
+    Higval: 150
+   }
+   ,
+   {
+    Name: 'A',
+    Curval: 100,
+    Lowval: 50,
+    Higval: 150
+   }
+   ,
+   {
+    Name: 'A',
+    Curval: 100,
+    Lowval: 50,
+    Higval: 150
+   }
+   ,
+   {
+    Name: 'A',
+    Curval: 100,
+    Lowval: 50,
+    Higval: 150
+   }
+   ,
+   {
+    Name: 'A',
+    Curval: 100,
+    Lowval: 50,
+    Higval: 150
+   }
+   ,
+   {
+    Name: 'A',
+    Curval: 100,
+    Lowval: 50,
+    Higval: 150
+   }
+   ,
+   {
+    Name: 'A',
+    Curval: 100,
+    Lowval: 50,
+    Higval: 150
+   }
 ];
 
   return (
-      
-    <body >
 
-    <table>
-      
-    <tr>
-      <Stockheader items = {Object.keys(ROWS[0])}/>
-    </tr>
+  <html className = "bg">  
+  <body>
 
-    <Stocklist items = {ROWS}/>
+    <div class="topnav">
+        <a class="active" href="#home">MANDEEP</a>
+        <a href="#about">SANJAY</a>
+        <a href="#contact">TARUN</a>
+        <input type="text" placeholder="Broo.." />
+    </div>
 
-    </table> 
+    <br />
 
-    </body>
+    <table  id="table">   
+      <tr>
+        <Stockheader items = {Object.keys(ROWS[0])}/>
+      </tr>
+        <Stocklist items = {ROWS}/>
+    </table>
+
+  </body>
+  </html>
 
   );
 }
